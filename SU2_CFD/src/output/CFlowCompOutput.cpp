be78982501292dad8e2ffda366eda0cd018dc83f
Filipi Kunz <38321697+FilipiKunz@users.noreply.github.com>
2024-02-06 18:27:25 +0000

    Update CFlowCompOutput.cpp

--- SU2_CFD/src/output/CFlowCompOutput.cpp
+++ SU2_CFD/src/output/CFlowCompOutput.cpp
@@ -298,6 +298,12 @@ void CFlowCompOutput::SetVolumeOutputFields(CConfig *config){
   if (config->GetTime_Domain()){
     SetTimeAveragedFields();
   }
+
+  AddVolumeOutput("NORMAL-X", "Normal_x", "NORMALS", "Surface normals");
+  AddVolumeOutput("NORMAL-Y", "Normal_y", "NORMALS", "Surface normals");
+  if (nDim == 3) AddVolumeOutput("NORMAL-Z", "Normal_z", "NORMALS", "Surface normals");
+  AddVolumeOutput("AREA", "Area", "NORMALS", "Surface normals");
+
 }
 
 void CFlowCompOutput::SetProbeOutputFields(CConfig *config, unsigned int nProbe){
